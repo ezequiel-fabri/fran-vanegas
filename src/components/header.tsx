cbf0e7fe58753346f7324fdf960b82251ca54035
Ezequiel Fabri <ezequiel.fabri@retiasoft.com>
2023-05-17 15:32:16 -0300

    adding new animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,11 +12,27 @@ const Header: React.FC = () => {
       </nav>
       <div className="min-w-[354px] absolute bottom-0 z-40 left-1/4">
         <Image
+          className="relative z-10"
           src="/fposing.png"
           alt="myself-in-cool-pose"
           height={538}
           width={354}
         />
+        {/* <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            left: 294,
+            width: "0px",
+            height: "0px",
+            borderStyle: "solid",
+            borderWidth: "0 0px 58px 425px",
+            borderColor:
+              "transparent transparent rgba(10, 0, 0, 0.4) transparent",
+            transform: "rotate(180deg)",
+            filter: "blur(5px)",
+          }}
+        /> */}
       </div>
       <div className="min-w-[788px] absolute bottom-0 left-1/4 max-[740px]:hidden z-0">
         <div className="absolute bottom-0 w-[350px] max-[740px]:hidden text-white flex flex-col h-[507px] right-[60px]">
